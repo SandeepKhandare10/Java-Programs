3d1e7e22cd4c0db4a521e64ffd280842afddcf54
SandeepKhandare10 <sandipkhandare10@gmail.com>
2024-06-11 12:24:58 +0530

    hiii

---
+++ src/loop_01.java
@@ -0,0 +1,19 @@
+public class loop_01 {
+    public static void main(String[] args) {
+//        for(int i=0;i<=10;i=i+1) {
+//            //  System.out.println("Hello world");
+//            System.out.println(i);
+//            int i = 0;
+//            while (i < 11) {
+//                System.out.println(i);
+//                i++;
+//            }
+//        }
+        int i=0;
+        do {
+            System.out.println(i);
+            i++;
+        }while (i<11);
+
+    }
+}
